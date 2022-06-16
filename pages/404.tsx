f77423f5a69ecaca4c2956e78a72c97f12af2310
oushihao <oushihao97@gmail.com>
2022-06-16 11:17:04 +0800

    feat: 404 page head
    Change-Id: I956f7c8e7dd62ad684b1615b9f15e6283ff18233

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import cssModule from '../styles/404.module.css'
+import Head from '../components/Head'
 
 const Custom404: NextPage = () => {
   return (
     <div className={cssModule.container}>
+      <Head title="404 Page Not Found" />
       <img className="w-full h-full" src="/404.svg" />
     </div>
   )
