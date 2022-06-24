eac4586422b4ab57ef31b17c4df9ee4eaaaa2a5a
Dewey Ou <oushihao97@gmail.com>
2022-06-24 23:35:39 +0800

    feat: logo click handle

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import useDark from '../../hooks/useDark'
 import Github from './Github'
 import ToggleTheme from './ToggleTheme'
@@ -6,12 +7,13 @@ import Posts from './Posts'
 
 const NavBar: NextPage = () => {
   const { isDark, toggleDark } = useDark()
+  const router = useRouter()
 
   return (
     <header className="p-8 flex flex-row justify-between items-center">
-      <a href="/">
+      <div className="cursor-pointer" onClick={() => router.push('/')}>
         <img src={isDark ? '/logo-dark.svg' : '/logo.svg'} className="w-10 h-10"></img>
-      </a>
+      </div>
       <div className="grid grid-flow-col gap-1.2rem">
         <Posts isDark={isDark} />
         <ToggleTheme
